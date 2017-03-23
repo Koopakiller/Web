9e578b45a533bce1f0e9c6a57ce87ceaeb6b36f3
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-24 00:57:11 +0100

    Deployment Script Improvement

--- Database/Database/Script.PostDeployment.sql
+++ Database/Database/Script.PostDeployment.sql
@@ -1,19 +1,19 @@
 ﻿
 -- HumanRelationKinds ----------------------------------------------------------
 
-IF NOT EXISTS(SELECT * FROM [HumanRelationKinds] AS hrk WHERE hrk.Name = 'Mother')
+IF NOT EXISTS(SELECT * FROM [HumanRelationKinds] AS hrk WHERE hrk.[Name] = 'Mother')
 BEGIN
 	INSERT INTO [HumanRelationKinds] ([Name]) VALUES ('Mother')
 END
-IF NOT EXISTS(SELECT * FROM [HumanRelationKinds] AS hrk WHERE hrk.Name = 'Father')
+IF NOT EXISTS(SELECT * FROM [HumanRelationKinds] AS hrk WHERE hrk.[Name] = 'Father')
 BEGIN
 	INSERT INTO [HumanRelationKinds] ([Name]) VALUES ('Father')
 END
-IF NOT EXISTS(SELECT * FROM [HumanRelationKinds] AS hrk WHERE hrk.Name = 'Brother')
+IF NOT EXISTS(SELECT * FROM [HumanRelationKinds] AS hrk WHERE hrk.[Name] = 'Brother')
 BEGIN
 	INSERT INTO [HumanRelationKinds] ([Name]) VALUES ('Brother')
 END
-IF NOT EXISTS(SELECT * FROM [HumanRelationKinds] AS hrk WHERE hrk.Name = 'Sister')
+IF NOT EXISTS(SELECT * FROM [HumanRelationKinds] AS hrk WHERE hrk.[Name] = 'Sister')
 BEGIN
 	INSERT INTO [HumanRelationKinds] ([Name]) VALUES ('Sister')
 END
