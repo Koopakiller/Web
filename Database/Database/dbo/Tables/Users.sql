074ecf924e9f4ecc63aeb5450305675b851e0e47
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-24 00:37:48 +0100

    Identity

--- Database/Database/dbo/Tables/Users.sql
+++ Database/Database/dbo/Tables/Users.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Users]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [PersonId] INT NOT NULL, 
     CONSTRAINT [FK_Users_Persons] FOREIGN KEY ([PersonId]) REFERENCES [Persons]([Id])
 )
