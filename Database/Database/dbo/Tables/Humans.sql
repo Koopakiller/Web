7763b53492473f638db161f013a49f6e33623e78
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-24 02:56:42 +0100

    Made Resumes more generic

--- Database/Database/dbo/Tables/Humans.sql
+++ Database/Database/dbo/Tables/Humans.sql
@@ -4,6 +4,8 @@
     [BirthPlaceId] INT NULL, 
     [BirthDateTime] DATETIME NULL, 
     [PersonId] INT NOT NULL, 
+    [ResumeId] INT NULL, 
     CONSTRAINT [FK_Humans_Persons] FOREIGN KEY ([PersonId]) REFERENCES [Persons]([Id]), 
-    CONSTRAINT [FK_Humans_Places_BirthPlace] FOREIGN KEY ([BirthPlaceId]) REFERENCES [Places]([Id])
+    CONSTRAINT [FK_Humans_Places_BirthPlace] FOREIGN KEY ([BirthPlaceId]) REFERENCES [Places]([Id]), 
+    CONSTRAINT [FK_Humans_Resume] FOREIGN KEY ([ResumeId]) REFERENCES [Resumes]([Id])
 )
