3122f9a825aa4569acc6e826c68d0ff907446047
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-24 01:08:10 +0100

    Added Currencies

---
+++ Database/Database/dbo/Tables/Currencies.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Currencies]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY 
+)
---
+++ Database/Database/dbo/Tables/CurrencyIdentifier.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[CurrencyIdentifier]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CurrencyId] INT NOT NULL, 
+    [Identifier] NVARCHAR(128) NOT NULL, 
+    CONSTRAINT [FK_CurrencyIdentifier_Currencies] FOREIGN KEY ([CurrencyId]) REFERENCES [Currencies]([Id])
+)
