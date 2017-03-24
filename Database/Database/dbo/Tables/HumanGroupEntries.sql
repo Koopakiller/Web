a0b4e8b8973e3e2bd630f7d93ca5a419202b5a26
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-24 02:48:41 +0100

    Added Human Groups

---
+++ Database/Database/dbo/Tables/HumanGroupEntries.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[HumanGroupEntries]
+(
+	[GroupId] INT NOT NULL , 
+    [HumanId] INT NOT NULL, 
+    PRIMARY KEY ([GroupId], [HumanId]), 
+    CONSTRAINT [FK_HumanGroupEntries_HumanGroups] FOREIGN KEY ([GroupId]) REFERENCES [HumanGroups]([Id]), 
+    CONSTRAINT [FK_HumanGroupEntries_Humans] FOREIGN KEY ([HumanId]) REFERENCES [Humans]([Id])
+)
---
+++ Database/Database/dbo/Tables/HumanGroups.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[HumanGroups]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Description] NVARCHAR(MAX) NOT NULL, 
+    [IsComplete] BIT NOT NULL 
+)
