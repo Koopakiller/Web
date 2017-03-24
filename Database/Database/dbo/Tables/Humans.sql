0d500bad8aef979c600250c8b1c54e9185cfe7c9
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-24 01:23:02 +0100

    Added Places

--- Database/Database/dbo/Tables/Humans.sql
+++ Database/Database/dbo/Tables/Humans.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[Humans]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [BirthPlace] NVARCHAR(128) NULL, 
+    [BirthPlaceId] INT NULL, 
     [BirthDateTime] DATETIME NULL, 
     [PersonId] INT NOT NULL, 
-    CONSTRAINT [FK_Humans_Persons] FOREIGN KEY ([PersonId]) REFERENCES [Persons]([Id])
+    CONSTRAINT [FK_Humans_Persons] FOREIGN KEY ([PersonId]) REFERENCES [Persons]([Id]), 
+    CONSTRAINT [FK_Humans_Places_BirthPlace] FOREIGN KEY ([BirthPlaceId]) REFERENCES [Places]([Id])
 )
---
+++ Database/Database/dbo/Tables/Places.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Places]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [Geography] GEOGRAPHY NULL
+)
