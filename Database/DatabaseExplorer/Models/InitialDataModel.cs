f47666207ae06f719291ab34e82aaa919efa92f4
Tom Lambert <tom.lambert.13@LIVE.COM>
2017-03-24 16:01:23 +0100

    Added InitialDataModel

---
+++ Database/DatabaseExplorer/Models/InitialDataModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseExplorer.Models
+{
+    public class InitialDataModel
+    {
+        public IList<String> TableColumns { get; set; }
+    }
+}
